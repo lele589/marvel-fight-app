2218a9cb17b9ba7c9dda7a8d2fa1c965a72c83b2
elenacarrasco <elenacfnb@gmail.com>
2022-02-22 09:22:32 +0100

    add custom fonts to assets

---
+++ src/utils/fonts.ts
@@ -0,0 +1,19 @@
+import { TextStyle } from 'react-native'
+
+export const fonts: { [key: string]: TextStyle } = {
+  thin: {
+    fontFamily: 'Nunito-ExtraLight',
+  },
+  regular: {
+    fontFamily: 'Nunito-Regular',
+  },
+  medium: {
+    fontFamily: 'Nunito-SemiBold',
+  },
+  bold: {
+    fontFamily: 'Nunito-Bold',
+  },
+  heavy: {
+    fontFamily: 'Nunito-ExtraBold',
+  },
+}
